5654af6b5fab20ee36b59647f36e94e158e66968
Mohab <114109820+Mohabnasser74@users.noreply.github.com>
2023-01-24 01:21:36 +0200

    Delete web.js

--- web.js
+++
@@ -1,7 +0,0 @@
-let body = document.querySelector("body");
-
-if (navigator.userAgentData["platform"] === "Windows") {
-    body.style.backgroundColor = "black"
-} else {
-    body.style.backgroundColor = "red"
-}
